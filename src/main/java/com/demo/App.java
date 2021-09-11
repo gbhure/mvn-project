9e53389778af5563d4beeb3cef5cc9222bb881cc
Ganesh.Bhure <ganesh.bhure@gmail.com>
2021-09-11 16:59:37 +0530

    first commit

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,12 @@
+package com.demo;
+public class App 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new App().greet("world"));
+    }
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
